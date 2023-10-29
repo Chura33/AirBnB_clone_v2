d2b6474f765467cd31fbb732e5af0b2547f70b7b
Chura33 <churabrady@gmail.com>
2023-10-29 10:50:55 -0700

    solved task 1

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,13 +118,44 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        parts = args.split()
+        class_name = parts[0]
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[class_name]()
+
+        attr_dict = {}
+
+        for part in parts[1:]:
+            key_value = part.split('=')
+
+            if len(key_value) != 2:
+                print("Invalid parameter format:", part)
+                continue
+
+            key, value = key_value[0], key_value[1]
+
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1].replace('_', ' ')
+            elif '.' in value:
+                try:
+                    value = float(value)
+                except ValueError:
+                    print("Invalid float value:", part)
+                    continue
+            else:
+                try:
+                    value = int(value)
+                except ValueError:
+                    print("Invalid integer value:", part)
+                    continue
+            attr_dict[key] = value
+        for key, values in attr_dict.items():
+            setattr(new_instance, key, values)
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -319,6 +350,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
